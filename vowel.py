dfd3508b4cfe55064adba8dd094d29eb4be4aeb2
Jakub Folta <jakubfolta@yahoo.co.uk>
2018-10-22 13:58:06 +0200

    continuation and solution

--- vowel.py
+++ vowel.py
@@ -1,5 +1,18 @@
 def anti_vowel(text):
-  vowels = ''
+  vowels = 'a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U'
+  for x in vowels:
+    text = text.replace(x, '')
+  return (text)
+
+
+print (anti_vowel('AbeCadlo')  )
+
+
+def anti_vowel(text):
   for x in text:
-    if x isupper('a', 'e', )
-  
+    if x in ('a', 'e', 'i', 'o', 'u') or x in ('A', 'E', 'I', 'O', 'U') :
+      text = text.replace(x, '')
+  return (text)
+
+
+print (anti_vowel('AbeCadlo')  )

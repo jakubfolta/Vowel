c121ce460bed36e1047586224ca48f7731aeff22
Jakub Folta <jakubfolta@yahoo.co.uk>
2018-10-22 16:51:39 +0200

    simplification

--- vowel.py
+++ vowel.py
@@ -1,18 +1,19 @@
 def anti_vowel(text):
-  vowels = 'a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U'
+  vowels = 'aeiouAEIOU'
   for x in vowels:
     text = text.replace(x, '')
   return (text)
 
 
-print (anti_vowel('AbeCadlo')  )
+print (anti_vowel('AbeCadlo'))
 
 
 def anti_vowel(text):
   for x in text:
-    if x in ('a', 'e', 'i', 'o', 'u') or x in ('A', 'E', 'I', 'O', 'U') :
+    if x in ('aeiou') or x in ('AEIOU') :
       text = text.replace(x, '')
   return (text)
 
 
-print (anti_vowel('AbeCadlo')  )
+print (anti_vowel('AbeCadlo'))
+ 
